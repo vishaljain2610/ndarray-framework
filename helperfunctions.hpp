9ee90958bcd3410fcf5002b0d5e34759c9553377
Vishal Jain <jainvishal628@gmail.com>
2019-02-28 22:18:29 +0530

    Methods

---
+++ helperfunctions.hpp
@@ -0,0 +1,28 @@
+#ifndef HELPERFUNCTION_H
+#define HELPERFUNCTION_H
+
+#include <iostream>
+#include <array>
+#include <algorithm>
+#include <memory>
+#include <cstdarg>
+#include<string.h> 
+#include "slice.hpp"
+#include "ndarray.hpp"
+#include "arithematic.hpp"
+// #include "helperfunctions.hpp"
+// #include "methods.hpp"
+
+template<typename T, size_t num_dimensions> 
+array<T, (num_dimensions-1)> removeArrayElement(array<T, num_dimensions> a, size_t index){
+    array<T, (num_dimensions-1)> output;
+    for(size_t i =0,j=0;i<a.size();i++)
+        {          
+            if(i!=index)
+                output[j++] = a[i];
+        }
+    return output;
+
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -4,17 +4,37 @@
 
 #include "slice.hpp"
 #include "ndarray.hpp"
+#include "arithematic.hpp"
+#include "helperfunctions.hpp"
+#include "methods.hpp"
 
 using namespace std;
 
 void assignment_related(){
-	array<size_t, 3> shape3 = {3, 3, 3};
-	vector<int64_t> allocated_memory3(calc_size<size_t, 3>(shape3));
-	auto array3d = create_array<int64_t, 3>(allocated_memory3.data(), shape3);
-	array3d(2, 1, 1) = 2;
-	auto array2d = array3d[2];
-	auto array1d = array2d[1];
-	auto array1d2 = array3d[2][1];
+	array<size_t, 2> shape3 = {1,6};
+	vector<int64_t> allocated_memory3(calc_size<size_t, 2>(shape3));
+	auto array3d = create_array<int64_t, 2>(allocated_memory3.data(), shape3);
+	
+	for(size_t i = 0;i<array3d.total_size;i++){
+		array3d.data_buffer.get()[i] = i;
+	}
+	auto array3dsumoutput = transpose(array3d);
+	cout << array3dsumoutput.shape;
+		
+	for(size_t i = 0;i<array3dsumoutput.total_size;i++){
+		cout << array3dsumoutput.data_buffer.get()[i];
+	}
+	// auto array3dalloutput = byteswap(array3d);
+
+
+	// array<size_t, 3> index = {1,4,2};
+	
+	// array<size_t, 3> value = {67,4,21};
+	// string mode = "clip";
+	// put(array3d, index,value, mode);
+	// for(size_t i = 0;i<array3dalloutput.total_size;i++){
+	// 	cout <<array3dalloutput.data_buffer.get()[i] << " ";
+	// }
 }
 
 void slice_related(){
@@ -35,7 +55,7 @@ void slice_related(){
 int main()
 {	
 	assignment_related();
-	slice_related();
+	// slice_related();
 	return 0;
 }
 
